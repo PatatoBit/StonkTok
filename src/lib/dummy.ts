6f799ab519d1f2b91c1290430a00bfbbfedcfce0
PatatoBit <pathonscript@gmail.com>
2025-07-28 17:52:55 +0700

    added more dummy data points

--- src/lib/dummy.ts
+++ src/lib/dummy.ts
@@ -31,6 +31,7 @@ export const dummyInvestments: Investment[] = [
 ];
 
 export const dummySnapshots: VideoSnapshot[] = [
+	// First video snapshots
 	{
 		videoId: '1234567890',
 		likes: 60,
@@ -49,7 +50,81 @@ export const dummySnapshots: VideoSnapshot[] = [
 		comments: 22,
 		createdAt: new Date('2023-10-02T12:20:00Z').toISOString()
 	},
+	{
+		videoId: '1234567890',
+		likes: 130,
+		comments: 30,
+		createdAt: new Date('2023-10-02T12:30:00Z').toISOString()
+	},
+	{
+		videoId: '1234567890',
+		likes: 150,
+		comments: 35,
+		createdAt: new Date('2023-10-03T12:40:00Z').toISOString()
+	},
+	{
+		videoId: '1234567890',
+		likes: 180,
+		comments: 40,
+		createdAt: new Date('2023-10-03T12:50:00Z').toISOString()
+	},
+	{
+		videoId: '1234567890',
+		likes: 200,
+		comments: 50,
+		createdAt: new Date('2023-10-04T13:00:00Z').toISOString()
+	},
+	{
+		videoId: '1234567890',
+		likes: 220,
+		comments: 60,
+		createdAt: new Date('2023-10-04T13:10:00Z').toISOString()
+	},
+	{
+		videoId: '1234567890',
+		likes: 250,
+		comments: 70,
+		createdAt: new Date('2023-10-05T13:20:00Z').toISOString()
+	},
+	{
+		videoId: '1234567890',
+		likes: 300,
+		comments: 80,
+		createdAt: new Date('2023-10-05T13:30:00Z').toISOString()
+	},
+	{
+		videoId: '1234567890',
+		likes: 350,
+		comments: 90,
+		createdAt: new Date('2023-10-06T13:40:00Z').toISOString()
+	},
+	{
+		videoId: '1234567890',
+		likes: 400,
+		comments: 100,
+		createdAt: new Date('2023-10-06T13:50:00Z').toISOString()
+	},
 
+	{
+		videoId: '1234567890',
+		likes: 1000,
+		comments: 110,
+		createdAt: new Date('2023-10-07T14:00:00Z').toISOString()
+	},
+
+	// Second video snapshots
+	{
+		videoId: '0987654321',
+		likes: 90,
+		comments: 15,
+		createdAt: new Date('2023-10-02T12:00:00Z').toISOString()
+	},
+	{
+		videoId: '0987654321',
+		likes: 100,
+		comments: 20,
+		createdAt: new Date('2023-10-02T12:00:00Z').toISOString()
+	},
 	{
 		videoId: '0987654321',
 		likes: 120,
