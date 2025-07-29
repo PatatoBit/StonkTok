419a4ad3ef39e8ece9fe58b3d8d321afc54d0933
PatatoBit <pathonscript@gmail.com>
2025-07-29 23:19:24 +0700

    updated production email magic redirect link

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,20 @@
-import { supabase } from "./supabaseClient";
+import { supabase } from './supabaseClient';
+
+const redirectUrl =
+	process.env.NODE_ENV === 'development'
+		? 'http://localhost:5173/auth/callback'
+		: 'https://grow-a-video.patato.me/auth/callback';
 
 export async function signInWithEmail(email: string) {
-  const { data, error } = await supabase.auth.signInWithOtp({
-    email: email,
-    options: {
-			emailRedirectTo: `${window.location.origin}/auth/callback`,
-    },
-  })
+	const { data, error } = await supabase.auth.signInWithOtp({
+		email: email,
+		options: {
+			emailRedirectTo: redirectUrl
+		}
+	});
 
 	if (error) {
-		console.error("Error signing in:", error);
+		console.error('Error signing in:', error);
 		throw error;
 	}
 
